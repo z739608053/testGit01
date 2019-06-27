27b610eef8d4708ac5208ccd827c77279c616dfa
zhugang <739608053@qq.com>
2019-06-27 20:05:49 +0800

    dev分支中更新了

--- src/main/java/com/itheima/test/Demo01.java
+++ src/main/java/com/itheima/test/Demo01.java
@@ -4,5 +4,6 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("111");
           System.out.println("222");
+        System.out.println("dev分支中进行更新了主函数");
     }
 }
