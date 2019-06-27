c5d1e232118828980c59ca7b35c62eeb9949292c
zhugang <739608053@qq.com>
2019-06-27 20:33:52 +0800

    提交了dev分支中的主函数更新

--- src/main/java/com/itheima/test/Demo01.java
+++ src/main/java/com/itheima/test/Demo01.java
@@ -5,5 +5,6 @@ public class Demo01 {
         System.out.println("111");
         System.out.println("222");
         System.out.println("这是master分支");
+        System.out.println("dev进行更新");
     }
 }
