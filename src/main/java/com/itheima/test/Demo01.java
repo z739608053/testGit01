98f0eecff1301b526d80738282530d601455ecf8
zhugang <739608053@qq.com>
2019-06-27 20:19:08 +0800

    版本1

--- src/main/java/com/itheima/test/Demo01.java
+++ src/main/java/com/itheima/test/Demo01.java
@@ -2,9 +2,8 @@ package com.itheima.test;
 
 public class Demo01 {
     public static void main(String[] args) {
-        System.out.println("111");
+          System.out.println("111");
           System.out.println("222");
-//        System.out.println("master中更新了主函数"  );
-        System.out.println("dev分支中进行更新了主函数");
+          System.out.println("dev分支中进行更新了主函数");
     }
 }
